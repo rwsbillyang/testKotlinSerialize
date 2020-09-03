2bf36459c5bebac04aa3e79406111f73af9df1f1
BillYang <rwsbillyang@qq.com>
2020-09-03 16:01:36 +0800

    Add generic test

--- src/main/kotlin/TestGeneric.kt
+++ src/main/kotlin/TestGeneric.kt
@@ -8,16 +8,20 @@ data class Box3<T>(var code: String = "OK", var msg: String? = null, val data: T
 data class Person3(val name: String)
 
 /**
- * /**
- * 测试泛型
+ * Test box generic
+ *
+ * https://github.com/Kotlin/kotlinx.serialization/blob/master/docs/basic-serialization.md#generic-classes
  * */
- * */
-//https://github.com/Kotlin/kotlinx.serialization/blob/master/docs/basic-serialization.md#generic-classes
 fun testGeneric(){
     // {"code":"OK","msg":null,"data":{"name":"Tom"}}
     println(Json.encodeToString(Box3(data = Person3("Tom"))) )
 
     //{"code":"OK","msg":null,"data":[{"name":"Tom"},{"name":"Tom2"}]}
-    val str2 = Json.encodeToString(Box3(data = listOf(Person3("Tom"),Person3("Tom2"))))
-    println(str2)
+    println(Json.encodeToString(Box3(data = listOf(Person3("Tom"),Person3("Tom2")))))
+
+    //{"code":"OK","msg":null,"data":"box primitive String"}
+    println(Json.encodeToString(Box3(data = "box primitive String")))
+
+    //{"code":"OK","msg":null,"data":1.23}
+    println(Json.encodeToString(Box3(data = 1.23F)))
 }
